fda4938278334e3b921783810c6c769bee24ce1d
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2017-01-02 19:20:54 -0800

    hello world app is working

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var displayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,10 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func saySomethingTapped(_ sender: UIButton) {
+        displayLabel.text = "Hello World!"
+    }
 
 }
 
